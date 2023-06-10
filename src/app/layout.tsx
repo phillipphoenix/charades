d6d59a1eb1d7702100de53ce12ff25f010e403e7
Phillip Phoelich <pphoelich@gmail.com>
2023-06-10 20:18:56 +0200

    Fixed metadata location.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import { GameContextProvider } from "@/lib/contexts/GameProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Charades",
   description: "Play charades",
+  manifest: "/manifest.json",
+  themeColor: "#6A3DE8",
 };
 
 export default function RootLayout({
@@ -17,10 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="forest">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6A3DE8" />
-      </Head>
       <body className={inter.className}>
         <GameContextProvider>{children}</GameContextProvider>
       </body>
