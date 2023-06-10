941350fca66626c7427bb727f5b2cbaa2b8cfd20
Phillip Phoelich <pphoelich@gmail.com>
2023-06-10 20:07:30 +0200

    Working on making the website into a PWA.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,29 @@
-import { GameContextProvider } from '@/lib/contexts/GameProvider'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { GameContextProvider } from "@/lib/contexts/GameProvider";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Charades',
-  description: 'Play charades',
-}
+  title: "Charades",
+  description: "Play charades",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" data-theme="forest">
-      <body className={inter.className}><GameContextProvider>{children}</GameContextProvider></body>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#6A3DE8" />
+      </Head>
+      <body className={inter.className}>
+        <GameContextProvider>{children}</GameContextProvider>
+      </body>
     </html>
-  )
+  );
 }
